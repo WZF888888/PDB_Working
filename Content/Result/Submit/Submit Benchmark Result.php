a2a7bc20ec89f88be89a50bc080aec504eeaea87
WZF888888 <100253117+WZF888888@users.noreply.github.com>
2023-04-13 17:41:23 +1000

    update 0.0.0.1.3

--- Content/Result/Submit/Submit Benchmark Result.php
+++ Content/Result/Submit/Submit Benchmark Result.php
@@ -1,7 +1,14 @@
 <?php
     session_start();
-    if(isset()){
+    $CSS_Login_True = "";
+    $CSS_Login_False = "";
+    if(isset($_SESSION["Login"])){
+        if($_SESSION["Username"] != NULL){
 
+        }
+    }
+    else{
+        $CSS_Login_False = "style='Display:none'";
     }
 ?>
 <html>
@@ -21,7 +28,23 @@
                 <p>Hello User! Thank you for your interest in submitting your benchmark results with us. To do so, you'll need to log in first. However, if you have privacy concerns, we offer an option to submit the results anonymously.</p>
             </div>
             <div class="User-Login-OK">
-
+                <div class="Submit-Form">
+                    <form>
+                        <select>
+                            <option value = "Please Select" selected>Please Select</option>
+                            <option value = "CPUz 17.01.64">CPU-Z Benchmark Version 17.01.64</option>
+                            <option value = "CPUz 19.01.64B">CPU-Z Benchmark Version 19.01.64 (Beta)</option>
+                            <option value = "CPUz 19.01.64AVX2">CPU-Z Benchmark Version 19.01.64 AVX2 (Beta)</option>
+                            <option value = "Cinebench R23 Single">Cinebench R23 Single Core</option>
+                            <option value = "Cinebench R23 Multi">Cinebench R23 Multiple Core</option>
+                            <option value = "Cinebench R20 Single">Cinebench R20 Single Core</option>
+                            <option value = "Cinebench R20 Multi">Cinebench R20 Multiple Core</option>
+                            <option value = "Cinebench R15 Single">Cinebench R15 Single Core</option>
+                            <option value = "Cinebench R15 Multi">Cinebench R15 Multiple Core</option>
+                            <option value = "3DMark">3DMark</option>
+                        </select>
+                    </form>
+                </div>
             </div>
         </div>
         <footer>
